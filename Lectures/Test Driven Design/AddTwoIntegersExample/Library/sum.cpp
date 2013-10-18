d51a712d989e9a842c7d7e8f5e3ff180dbf2cbd5
Joshua Peterson <petersonjm1@gmail.com>
2013-10-18 06:20:57 -0400

    Finished code for the add two integers example.

--- Lectures/Test Driven Design/AddTwoIntegersExample/Library/sum.cpp
+++ Lectures/Test Driven Design/AddTwoIntegersExample/Library/sum.cpp
@@ -1,6 +1,16 @@
+#include <stdexcept>
 #include "sum.h"
 
 namespace library
 {
+	int sum(int left, int right)
+	{
+		if (left < 0 || right < 0)
+		{
+			auto input_name = left < 0 ? "left" : "right";
+			throw std::invalid_argument(std::string("The ") + input_name + std::string(" input cannot be negative."));
+		}
+		return left + right;
+	}
 }
 
