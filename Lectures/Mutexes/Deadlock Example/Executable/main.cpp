52dc230834d50f3d3a55b0052fb63752822a9dc6
Joshua Peterson <petersonjm1@gmail.com>
2013-11-11 06:46:41 -0500

    Complete mutexes lecture
    - Add a deadlock example
    - Summarize the guidelines for the lecture

---
+++ Lectures/Mutexes/Deadlock Example/Executable/main.cpp
@@ -0,0 +1,39 @@
+#include <iostream>
+#include <mutex>
+#include <thread>
+
+std::mutex mutex1;
+std::mutex mutex2;
+
+void thread1()
+{
+    std::lock_guard<std::mutex> lock1(mutex1);
+
+    // Simulate some non-trivial execution.
+    std::this_thread::sleep_for(std::chrono::milliseconds(2000));
+    
+    std::lock_guard<std::mutex> lock2(mutex2);
+
+    std::cout << "thread 1\n";
+}
+
+void thread2()
+{
+    std::lock_guard<std::mutex> lock2(mutex2);
+    std::lock_guard<std::mutex> lock1(mutex1);
+
+    std::cout << "thread 2\n";
+}
+
+
+int main()
+{
+    std::thread t1(thread1);
+    std::thread t2(thread2);
+
+    t1.detach();
+    t2.detach();
+
+    std::cout << "Press any key to continue...\n";
+    std::getchar();
+}
