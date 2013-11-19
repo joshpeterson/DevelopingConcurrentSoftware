638381dbe00d9816cd91307e48572395ccfa8adf
Joshua Peterson <petersonjm1@gmail.com>
2013-11-19 07:03:01 -0500

    Add details about std::async.

---
+++ Lectures/Asynchronous Operations/AsyncGauss/Executable/main.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+#include <future>
+
+int gauss(int start, int end)
+{
+    int sum = 0;
+    for (auto i = start; i <= end; ++i)
+        sum += i;
+
+    return sum;
+}
+
+int main()
+{
+    std::future<int> sum = std::async(gauss, 1, 10000);
+
+    std::cout << "Sum: " << sum.get() << std::endl;
+
+    std::cout << "Press any key to continue...\n";
+    std::getchar();
+}
