afbd494aa2fd03322e4cf632500f014582df6d37
Joshua Peterson <petersonjm1@gmail.com>
2013-12-24 08:20:54 -0500

    Replace tabs with spaces.

--- Lectures/Case Study - Atomics and Object Lifetime/ReferenceCounting/Executable/main.cpp
+++ Lectures/Case Study - Atomics and Object Lifetime/ReferenceCounting/Executable/main.cpp
@@ -5,21 +5,21 @@
 
 void releaser(reference_counted<int>* tester, int call_identifier)
 {
-	tester->release(call_identifier);
+    tester->release(call_identifier);
 }
 
 int main()
 {
-	reference_counted<int> tester(42);
+    reference_counted<int> tester(42);
 
-	// Increment reference count so multiple threads will have time to launch.
-	for (auto i = 0; i < 1000; ++i)
-		tester.add_reference();
+    // Increment reference count so multiple threads will have time to launch.
+    for (auto i = 0; i < 1000; ++i)
+        tester.add_reference();
 
-	for (auto i = 0; i < 10000; ++i)
-		std::thread(&releaser, &tester, i).detach();
+    for (auto i = 0; i < 10000; ++i)
+        std::thread(&releaser, &tester, i).detach();
 
-	std::this_thread::sleep_for(std::chrono::seconds(5));
+    std::this_thread::sleep_for(std::chrono::seconds(5));
 
     std::cout << "Press any key to continue...\n";
     std::getchar();
