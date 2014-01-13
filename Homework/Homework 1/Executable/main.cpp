e2a42e8d239cda6488c339be5faea96fee239a1c
Joshua Peterson <petersonjm1@gmail.com>
2014-01-12 20:54:57 -0500

    Complete Homework assignment 1.

--- Homework/Homework 1/Executable/main.cpp
+++ Homework/Homework 1/Executable/main.cpp
@@ -1,4 +1,54 @@
+#include <iostream>
+#include <string>
+#include <functional>
+#include <stdexcept>
+#include "string_calculator.h"
+
+void validate(std::string, std::string, int, std::function<int(std::string, std::string)>, std::string);
+template<typename ExceptionType>
+void validate_exception(std::string, std::string, int, std::function<int(std::string, std::string)>, std::string, std::string);
 
 int main()
 {
+    validate("1", "6", 7, add, "+");
+    validate("-2", "13", 11, add, "+");
+    validate("6", "3", 3, subtract, "-");
+    validate("38", "-4", 42, subtract, "-");
+    validate("12", "5", 60, multiply, "*");
+    validate("-3", "6", 18, multiply, "*");
+    validate("4", "2", 2, divide, "/");
+    validate("13", "3", 4, divide, "/");
+    validate_exception<std::invalid_argument>("10", "0", 4, divide, "/", "invalid_argument");
+
+    std::cout << "Press any key to continue...\n";
+    std::getchar();
+
+    return 0;
 }
+
+void validate(std::string left, std::string right, int expected_result, std::function<int(std::string, std::string)> function_to_validate, std::string op)
+{
+    auto actual_result = function_to_validate(left, right);
+    if (actual_result == expected_result)
+        std::cout << left << op << right << " correct\n";
+    else
+        std::cout << left << op << right << " incorrect - expected: " << expected_result << " actual: " << actual_result << std::endl;
+}
+
+template<typename ExceptionType>
+void validate_exception(std::string left, std::string right, int expected_result, std::function<int(std::string, std::string)> function_to_validate, std::string op, std::string exception_type)
+{
+    try
+    {
+        function_to_validate(left, right);
+    }
+    catch (const ExceptionType&)
+    {
+        std::cout << left << op << right << " correct - exception caught\n";
+        return;
+    }
+
+    std::cout << left << op << right << " incorrect - expected an exception of type: " << exception_type << std::endl;
+}
+
+
--- Homework/Homework 1/Homework1Description.md
+++ Homework/Homework 1/Homework1Description.md
@@ -1,20 +1,21 @@
 Homework 1
 ==========
-This homeowork assignment requires you to write a string calculator using test driven design (TDD). The actual implementation of the string calculator is rather simple. The intent of this homework assignment is to focus on the practice of TDD.
+This homework assignment requires you to write a string calculator using test driven design (TDD). The actual implementation of the string calculator is rather simple. The intent of this homework assignment is to focus on the practice of TDD.
 
 Due: Prior to class on January 27
 
-Staring with the initial source code in Homework1.sln:
+Starting with the initial source code in Homework1.sln:
 
 1. Implement the four functions of the string calculator, add, subtract, multiply, divide.
 2. Each function accepts its left and right arguments as strings.
 3. Assume each string is an integer.
+4. The code in `main()` will be used to verify the correct behavior of the four functions. Each validation should be correct.
 
 You will be graded on:
 
 1. The correctness of the four functions as measured by the test in the main() function.
-2. The organization and sturcture of the test code.
+2. The organization and structure of the test code.
 
-This assignment is intended to be collaboritve. You may work on it with one or two other students in the course, and provide one implementation for the group. You are also encouraged to submit your finished work prior to the due date and receive feedback from me.
+This assignment is intended to be collaborative. You may work on it with one or two other students in the course, and provide one implementation for the group. You are also encouraged to submit your finished work prior to the due date and receive feedback from me.
 
 You should submit your finished work as a pull request on Github.
