3658b051463bf3e7e5ba35a2d6bb8ffad6a1693c
Joshua Peterson <petersonjm1@gmail.com>
2014-01-12 21:19:09 -0500

    Homework 1 solution.

--- Homework/Homework 1/Tests/string_calculator_tests.cpp
+++ Homework/Homework 1/Tests/string_calculator_tests.cpp
@@ -1,7 +1,48 @@
+#include <stdexcept>
 #include "gtest/gtest.h"
 #include "../Library/string_calculator.h"
 
-TEST(StringCaclulatorTests, SumOfZeroAndZeroIsZero)
+TEST(StringCalculatorTests, SumOfZeroAndZeroIsZero)
 {
     ASSERT_EQ(0, add("0", "0"));
+}
+
+TEST(StringCalculatorTests, SumOfOneAndZeroIsOne)
+{
+    ASSERT_EQ(1, add("1", "0"));
+}
+
+TEST(StringCalculatorTests, SumOfZeroAndOneIsOne)
+{
+    ASSERT_EQ(1, add("0", "1"));
+}
+
+TEST(StringCalculatorTests, SumOfZeroAndNegativeOneIsNegativeOne)
+{
+    ASSERT_EQ(-1, add("0", "-1"));
+}
+
+TEST(StringCalculatorTests, DifferenceOfThreeAndTwoIsOne)
+{
+    ASSERT_EQ(1, subtract("3", "2"));
+}
+
+TEST(StringCalculatorTests, DifferenceOfTwoAndThreeIsNegativeOne)
+{
+    ASSERT_EQ(-1, subtract("2", "3"));
+}
+
+TEST(StringCalculatorTests, ProductOfTwoAndThreeIsSix)
+{
+    ASSERT_EQ(6, multiply("2", "3"));
+}
+
+TEST(StringCalculatorTests, QuotientOfSixAndThreeIsTwo)
+{
+    ASSERT_EQ(2, divide("6", "3"));
+}
+
+TEST(StringCalculatorTests, DivideThrowsInvalidArgumentExceptionWhenRightIsZero)
+{
+    ASSERT_THROW(divide("6", "0"), std::invalid_argument);
 }
