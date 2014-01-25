1b69c2c74bffab913f3af7b768698f16feaadb97
DrehmanTJ1 <DrehmanTJ1@gcc.edu>
2014-01-25 15:28:21 -0500

    Homework 1 version 1

--- Homework/Homework 1/Library/string_calculator.cpp
+++ Homework/Homework 1/Library/string_calculator.cpp
@@ -3,20 +3,39 @@
 
 int add(const std::string& left, const std::string& right)
 {
-    return 0;
+	int leftInt;
+	int rightInt;
+	leftInt = atoi(left.c_str());
+	rightInt = atoi(right.c_str());
+    return leftInt+rightInt;
 }
 
 int subtract(const std::string& left, const std::string& right)
 {
-    return 0;
+	int leftInt;
+	int rightInt;
+	leftInt = atoi(left.c_str());
+	rightInt = atoi(right.c_str());
+	return leftInt-rightInt;
 }
 
 int multiply(const std::string& left, const std::string& right)
 {
-    return 0;
+	int leftInt;
+	int rightInt;
+	leftInt = atoi(left.c_str());
+	rightInt = atoi(right.c_str());
+	return leftInt*rightInt;
 }
 
 int divide(const std::string& left, const std::string& right)
 {
-    return 0;
+	int leftInt;
+	int rightInt;
+	leftInt = atoi(left.c_str());
+	rightInt = atoi(right.c_str());
+	if (rightInt != 0)
+		return leftInt / rightInt;
+	else
+		throw(std::invalid_argument("Divide by 0 error"));
 }
--- Homework/Homework 1/Tests/string_calculator_tests.cpp
+++ Homework/Homework 1/Tests/string_calculator_tests.cpp
@@ -5,3 +5,56 @@ TEST(StringCalculatorTests, SumOfZeroAndZeroIsZero)
 {
     ASSERT_EQ(0, add("0", "0"));
 }
+TEST(StringCalculatorTests, SumOfOneAndZeroIsOne)
+{
+	ASSERT_EQ(1, add("1", "0"));
+}
+TEST(StringCalculatorTests, SumOfOneAndOneIsTwo)
+{
+	ASSERT_EQ(2, add("1", "1"));
+}
+
+TEST(StringCalculatorTests, DifferenceOfZeroAndZeroIsZero)
+{
+	ASSERT_EQ(0, subtract("0", "0"));
+}
+TEST(StringCalculatorTests, DifferenceOfOneAndZeroIsOne)
+{
+	ASSERT_EQ(1, subtract("1", "0"));
+}
+TEST(StringCalculatorTests, DifferenceOfOneAndOneIsZero)
+{
+	ASSERT_EQ(0, subtract("1", "1"));
+}
+TEST(StringCalculatorTests, DifferenceOfZeroAndOneIsNegativeOne)
+{
+	ASSERT_EQ(-1, subtract("0", "1"));
+}
+
+
+TEST(StringCalculatorTests, ProductOfZeroAndZeroIsZero)
+{
+	ASSERT_EQ(0, multiply("0", "0"));
+}
+TEST(StringCalculatorTests, ProductOfOneAndZeroIsZero)
+{
+	ASSERT_EQ(0, multiply("1", "0"));
+}
+TEST(StringCalculatorTests, ProductOfOneAndOneIsOne)
+{
+	ASSERT_EQ(1, multiply("1", "1"));
+}
+
+TEST(StringCalculatorTests, QuotientOfZeroAndOneIsZero)
+{
+	ASSERT_EQ(0, divide("0", "1"));
+}
+TEST(StringCalculatorTests, QuotientOfOneAndOneIsOne)
+{
+	ASSERT_EQ(1, divide("1", "1"));
+}
+
+TEST(StringCalculatorTests, DifferenceOfOneAndZeroIsUndefined)
+{
+	ASSERT_ANY_THROW(divide("1", "0"));
+}
