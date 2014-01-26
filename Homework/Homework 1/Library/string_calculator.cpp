9ddbfc86e2a3ff5388c15627d35e97ce152db22c
jumonza <jumonza@gmail.com>
2014-01-26 13:33:56 -0500

    Homework 1
    Implementation of string calculator

--- Homework/Homework 1/Library/string_calculator.cpp
+++ Homework/Homework 1/Library/string_calculator.cpp
@@ -1,22 +1,58 @@
 #include <string>
 #include "string_calculator.h"
+#include <exception>
+
 
 int add(const std::string& left, const std::string& right)
 {
-    return 0;
+	int left_int; int right_int;
+
+	// string to int
+	left_int = std::stoi(left);
+	right_int = std::stoi(right);
+
+	// addition
+    return left_int+right_int;
 }
 
 int subtract(const std::string& left, const std::string& right)
 {
-    return 0;
+	int left_int; int right_int;
+
+	// string to int
+	left_int = std::stoi(left);
+	right_int = std::stoi(right);
+
+	// subtraction
+	return (left_int - right_int);
 }
 
 int multiply(const std::string& left, const std::string& right)
 {
-    return 0;
+	int left_int, right_int;
+
+	// string to int
+	left_int = std::stoi(left);
+	right_int = std::stoi(right);
+
+	// multiplication
+    return (left_int*right_int);
 }
 
 int divide(const std::string& left, const std::string& right)
 {
-    return 0;
+	int left_int, right_int, result;
+
+	// String to Int
+	left_int = std::stoi(left);
+	right_int = std::stoi(right);
+
+	// Exception checking
+	if (right_int == 0)
+	{
+		throw std::invalid_argument("Divide by Zero!");
+	}
+	
+	// Division
+	return (left_int / right_int);
 }
--- Homework/Homework 1/Tests/string_calculator_tests.cpp
+++ Homework/Homework 1/Tests/string_calculator_tests.cpp
@@ -1,7 +1,66 @@
 #include "gtest/gtest.h"
 #include "../Library/string_calculator.h"
 
+// Addition Tests
 TEST(StringCalculatorTests, SumOfZeroAndZeroIsZero)
 {
     ASSERT_EQ(0, add("0", "0"));
+}
+
+TEST(StringCalculatorTests, SumOfOneAndZeroIsOne)
+{
+	ASSERT_EQ(1, add("1", "0"));
+}
+
+TEST(StringCalculatorTests, SumOfZeroAndOneIsOne)
+{
+	ASSERT_EQ(1, add("0", "1"));
+}
+
+TEST(StringCalculatorTests, SumOfOneAndOneIsTwo)
+{
+	ASSERT_EQ(2, add("1", "1"));
+}
+
+// Subtraction Tests
+TEST(StringCalcultor, SubtractionOfNull)
+{
+	ASSERT_EQ(0, subtract("0", "0"));
+}
+
+TEST(StringCalculator, SubtractionOfOneAndZero)
+{
+	ASSERT_EQ(1, subtract("1", "0"));
+}
+
+TEST(StringCalculator, SubtractionOfZeroAndOne)
+{
+	ASSERT_EQ(-1, subtract("0", "1"));
+}
+
+// Multiplication Tests
+TEST(StringCalculator, MultiplicationZeroAndZero)
+{
+	ASSERT_EQ(0, multiply("0", "0"));
+}
+
+TEST(StringCalculator, MultiplicationOneAndOne)
+{
+	ASSERT_EQ(1, multiply("1", "1"));
+}
+
+// Division Tests
+TEST(StringCalculator, DivisionByOne)
+{
+	ASSERT_EQ(1, divide("1", "1"));
+}
+
+TEST(StringCalculator, DivisionByNegativeOne)
+{
+	ASSERT_EQ(-1, divide("1", "-1"));
+}
+
+TEST(StringCalculator, DivisionByZero)
+{
+	ASSERT_THROW(divide("1", "0"), std::invalid_argument);
 }
