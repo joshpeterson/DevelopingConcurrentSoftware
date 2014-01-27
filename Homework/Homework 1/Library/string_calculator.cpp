12cb961bc204dca3c78600cbf3cf5e2c410ed3d8
GilmerKE <kathleengilmer@gmail.com>
2014-01-27 18:18:52 -0500

    My homework 1

--- Homework/Homework 1/Library/string_calculator.cpp
+++ Homework/Homework 1/Library/string_calculator.cpp
@@ -3,20 +3,30 @@
 
 int add(const std::string& left, const std::string& right)
 {
-    return 0;
+	int r = atoi(right.c_str());
+	int l = atoi(left.c_str());
+	return r+l;
 }
 
 int subtract(const std::string& left, const std::string& right)
 {
-    return 0;
+	int l = atoi(left.c_str());
+	int r = atoi(right.c_str());
+	return l-r;
 }
 
 int multiply(const std::string& left, const std::string& right)
 {
-    return 0;
+	int l = atoi(left.c_str());
+	int r = atoi(right.c_str());
+	return l*r;
 }
 
 int divide(const std::string& left, const std::string& right)
 {
-    return 0;
+	int l = atoi(left.c_str());
+	int r = atoi(right.c_str());
+	if (r == 0)
+		throw r;
+	return l / r;	
 }
--- Homework/Homework 1/Tests/string_calculator_tests.cpp
+++ Homework/Homework 1/Tests/string_calculator_tests.cpp
@@ -5,3 +5,75 @@ TEST(StringCalculatorTests, SumOfZeroAndZeroIsZero)
 {
     ASSERT_EQ(0, add("0", "0"));
 }
+
+TEST(StringCalculatorTests, SumOfZeroAnd7Is7)
+{
+	ASSERT_EQ(7, add("0", "7"));
+}
+
+TEST(StringCalculatorTests, SumOf4And7Is11)
+{
+	ASSERT_EQ(11, add("4", "7"));
+}
+
+TEST(StringCalculatorTests, SumOfnegative4and7is3)
+{
+	ASSERT_EQ(3, add("-4", "7"));
+}
+
+TEST(StringCalculatorTests, DifferenceOfZeroAndZeroIsZero)
+{
+	ASSERT_EQ(0, subtract("0", "0"));
+}
+
+TEST(StringCalculatorTests, DifferenceOf7AndZeroIs7)
+{
+	ASSERT_EQ(7, subtract("7", "0"));
+}
+
+TEST(StringCalculatorTests, DifferenceOf7And4Is3)
+{
+	ASSERT_EQ(3, subtract("7", "4"));
+}
+
+TEST(StringCalculatorTests, ProductOfZeroAndZeroIs0)
+{
+	ASSERT_EQ(0, multiply("0", "0"));
+}
+
+TEST(StringCalculatorTests, ProductOfZeroAnd7Is0)
+{
+	ASSERT_EQ(0, multiply("0", "7"));
+}
+
+TEST(StringCalculatorTests, ProductOf4And7Is28)
+{
+	ASSERT_EQ(28, multiply("4", "7"));
+}
+
+TEST(StringCalculatorTests, ProductOfNegative4And7IsNeg28)
+{
+	ASSERT_EQ(-28, multiply("-4", "7"));
+}
+
+TEST(StringCalculatorTests, DivisionOfOneAndOneIs1)
+{
+	ASSERT_EQ(1, divide("1", "1"));
+}
+
+TEST(StringCalculatorTests, DivisionOf10And5Is2)
+{
+	ASSERT_EQ(2, divide("10", "5"));
+}
+
+TEST(StringCalculatorTests, DivisionOfNeg10and5IsNeg2)
+{
+	ASSERT_EQ(-2, divide("-10", "5"));
+}
+
+TEST(StringCalculatorTests, DivisionOf7ByZeroIsThrow)
+{
+	ASSERT_ANY_THROW(divide("7", "0"));
+}
+
+
