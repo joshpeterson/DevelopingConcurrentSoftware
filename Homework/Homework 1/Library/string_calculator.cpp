eb487c04c9d7d3388e51c6f8707bf6902c5e5569
Jon Ousley <jousley93@gmail.com>
2014-01-27 18:09:29 -0500

    homework 1 complete

--- Homework/Homework 1/Library/string_calculator.cpp
+++ Homework/Homework 1/Library/string_calculator.cpp
@@ -3,20 +3,23 @@
 
 int add(const std::string& left, const std::string& right)
 {
-    return 0;
+	return atoi(left.c_str()) + atoi(right.c_str());
 }
 
 int subtract(const std::string& left, const std::string& right)
 {
-    return 0;
+	return atoi(left.c_str()) - atoi(right.c_str());
 }
 
 int multiply(const std::string& left, const std::string& right)
 {
-    return 0;
+	return atoi(left.c_str()) * atoi(right.c_str());
 }
 
 int divide(const std::string& left, const std::string& right)
 {
-    return 0;
+	int divisor = atoi(right.c_str());
+	if (divisor == 0)
+		throw std::domain_error("Divide by zero exception");
+	return atoi(left.c_str()) / divisor;
 }
