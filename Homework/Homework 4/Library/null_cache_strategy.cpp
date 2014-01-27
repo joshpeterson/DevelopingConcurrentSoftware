0e41c49c06918fee05e7c9df424d6260f530a04f
Joshua Peterson <petersonjm1@gmail.com>
2014-01-27 06:06:56 -0500

    Remove old homework 4.

--- Homework/Homework 4/Library/null_cache_strategy.cpp
+++
@@ -1,6 +0,0 @@
-#include "null_cache_strategy.h"
-
-std::unique_ptr<cache_strategy_helper> null_cache_strategy::during_cache_access()
-{
-    return std::unique_ptr<cache_strategy_helper>(new cache_strategy_helper);
-}
