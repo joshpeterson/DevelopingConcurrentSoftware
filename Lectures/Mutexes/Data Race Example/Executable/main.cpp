81a1448ca4cbb4f6fb63389ddd3d3916d32c3879
DrehmanTJ1 <DrehmanTJ1@gcc.edu>
2014-01-27 20:21:05 -0500

    Notes on lecture

--- Lectures/Mutexes/Data Race Example/Executable/main.cpp
+++ Lectures/Mutexes/Data Race Example/Executable/main.cpp
@@ -1,17 +1,23 @@
 #include <iostream>
 #include <thread>
+#include <mutex>
 
+std::mutex m;
 int shared;
 
 void reader()
 {
+	m.lock();
     std::cout << "The value of shared is: " << shared << std::endl;
+	m.unlock();
 }
 
 void writer()
 {
+	m.lock();
     for (int i = 0; i < 1000000; ++i)
         shared++;
+	m.unlock();
 }
 
 int main()
