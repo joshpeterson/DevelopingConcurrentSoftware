4923f297a891d78b27094e50335280e6964bf411
DrehmanTJ1 <DrehmanTJ1@gcc.edu>
2014-02-02 17:03:03 -0500

    HW 2 updated

--- Homework/Homework 2/Library/thread_safe_vector.h
+++ Homework/Homework 2/Library/thread_safe_vector.h
@@ -1,30 +1,42 @@
+#include <vector>
+#include <mutex>
+
 #ifndef __THREAD_SAFE_VECTOR_H
 #define __THREAD_SAFE_VECTOR_H
 
-template <typename T>
+template <typename T, typename M>
 class thread_safe_vector
 {
 public:
     void push_back(T value)
     {
-		position[Counter] = value;
-		Counter++;
+		std::lock_guard<M>lock(mutex1);		//Vector locked, no other threads can now use the vector
+		vector1.push_back(value);
+		std::lock_guard<M>unlock(mutex1);	//Vector unlocked, other threads can now use the vector
 		return;
     }
 
     T at(int index) const
     {
-		return position[index];
+		std::lock_guard<M>lock(mutex1);
+		T temp = vector1[index];
+		std::lock_guard<M>unlock(mutex1);
+		return temp;
     }
 
     int size() const
     {
-        return Counter;
+		std::lock_guard<M>lock(mutex1);
+		T temp = vector1.size();
+		std::lock_guard<M>unlock(mutex1);
+		return temp;
     }
 	
 private:
-	T position[1000];
-	int Counter = 0;
+//	T position[1000];
+	std::vector<T> vector1;
+	mutable M mutex1;
+//	int Counter = 0;
 };
 
 #endif //__THREAD_SAFE_VECTOR_H
---
+++ Homework/Homework 2/Tests/mock_mutex.cpp
@@ -0,0 +1,6 @@
+// This was copied from the example code QueueExample
+
+#include "mock_mutex.h"
+
+bool mock_mutex::lock_called_ = false;
+bool mock_mutex::unlock_called_ = false;
