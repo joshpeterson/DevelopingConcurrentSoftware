75e40c6c649a99e7905e2331acd77815ef4aff0e
Jon Ousley <jousley93@gmail.com>
2014-02-03 17:58:56 -0500

    homework 2 completed

--- Homework/Homework 2/Library/thread_safe_vector.h
+++ Homework/Homework 2/Library/thread_safe_vector.h
@@ -1,23 +1,34 @@
 #ifndef __THREAD_SAFE_VECTOR_H
 #define __THREAD_SAFE_VECTOR_H
 
+#include <vector>
+#include <mutex>
+
 template <typename T>
 class thread_safe_vector
 {
 public:
-    void push_back(T value)
-    {
-    }
+	void push_back(T value)
+	{
+		std::lock_guard<std::mutex> guard(mutex_);
+		v_.push_back(value);
+	}
 
-    T at(int index) const
-    {
-        return T();
-    }
+	T at(int index) const
+	{
+		std::lock_guard<std::mutex> guard(mutex_);
+		return v_.at(index);
+	}
 
-    int size() const
-    {
-        return 0;
-    }
+	int size() const
+	{
+		std::lock_guard<std::mutex> guard(mutex_);
+		return v_.size();
+	}
+
+private:
+	std::vector<T> v_;
+	mutable std::mutex mutex_;
 };
 
 #endif //__THREAD_SAFE_VECTOR_H
