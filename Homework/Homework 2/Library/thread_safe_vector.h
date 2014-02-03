d8891a88d1f16865e544a978958442fe01e7ed9d
unknown <KLOOSTERMANCW1@2CE13206MH.GCC.edu>
2014-02-03 18:16:37 -0500

    HW 2 (worked with pfeiffer & Drehman)

--- Homework/Homework 2/Library/thread_safe_vector.h
+++ Homework/Homework 2/Library/thread_safe_vector.h
@@ -1,23 +1,35 @@
 #ifndef __THREAD_SAFE_VECTOR_H
 #define __THREAD_SAFE_VECTOR_H
-
+#include <vector>
+#include <mutex>
 template <typename T>
 class thread_safe_vector
 {
 public:
+
+	thread_safe_vector(std::vector<T> vect) : vec(vect) {}
+	thread_safe_vector() { };
+
     void push_back(T value)
     {
+		std::lock_guard<std::mutex> g(mm);
+		vec.push_back(value);
     }
 
     T at(int index) const
     {
-        return T();
+		std::lock_guard<std::mutex> g(mm);
+        return vec.at(index);
     }
 
     int size() const
     {
-        return 0;
-    }
+		std::lock_guard<std::mutex> g(mm);
+		return vec.size();
+	}
+private:
+		mutable std::mutex mm;
+		std::vector<T> vec;
 };
 
 #endif //__THREAD_SAFE_VECTOR_H
