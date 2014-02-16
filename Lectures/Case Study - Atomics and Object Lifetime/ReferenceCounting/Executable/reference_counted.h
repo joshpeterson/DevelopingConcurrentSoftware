48cbc85a0588ea64c15dc4be1b045c59d2cd8432
DrehmanTJ1 <DrehmanTJ1@gcc.edu>
2014-02-16 02:37:38 -0500

    Lecture 4

--- Lectures/Case Study - Atomics and Object Lifetime/ReferenceCounting/Executable/reference_counted.h
+++ Lectures/Case Study - Atomics and Object Lifetime/ReferenceCounting/Executable/reference_counted.h
@@ -3,6 +3,7 @@
 
 #include <atomic>
 #include <iostream>
+//#include <chrono>
 
 template <typename T>
 class reference_counted
@@ -21,9 +22,13 @@ public:
 
     void release(int call_identifier)
     {
-        --count_;           // 1
-        if (count_ == 0)    // 2
-            delete value_;
+        auto count = --count_;           // 1
+//		std::this_thread::sleep_for(std::chrono::milliseconds(1000));
+		if (count == 0)    // 2
+		{
+//			std::cout << call_identifier;
+			delete value_;
+		}
     }
 
 private:
