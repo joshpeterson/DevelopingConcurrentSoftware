f51b9d7f94f52a07b7fef73514cb09bb44c08b74
Jon Ousley <jousley93@gmail.com>
2014-02-24 16:58:02 -0500

    homework 5

--- Homework/Homework 5/Library/mpi_word_counter.cpp
+++ Homework/Homework 5/Library/mpi_word_counter.cpp
@@ -1,19 +1,42 @@
 #include "mpi_word_counter.h"
+#include <iostream>
 
 mpi_word_counter::mpi_word_counter(const std::string& word_to_find, const std::vector<std::string>& strings_to_search)
 {
+	word = word_to_find;
+	search_body = strings_to_search;
 }
 
 void mpi_word_counter::map(unsigned int begin, unsigned int end)
 {
+	for (auto i = begin; i <= end; i++)
+	{
+		if (equalsSearchTerm(i)) count++;
+	}
 }
 
 unsigned int mpi_word_counter::reduce(unsigned int input)
 {
-    return 0;
+    count+=input;
+	return count;
 }
 
 int mpi_word_counter::get_count() const
 {
-    return 0;
+    return count;
+}
+
+bool mpi_word_counter::equalsSearchTerm(unsigned int index)
+{
+	std::string curString = search_body[index];
+	unsigned int size = word.size();
+
+	if (curString.size() != size)
+		return false;
+
+	for (unsigned int i = 0; i < size; i++)
+		if (tolower(word[i]) != tolower(curString[i]))
+			return false;
+
+	return true;
 }
